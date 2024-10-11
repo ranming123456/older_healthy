30ab0fd76f15eb15da3f47f5c57eee7ebac79f0b
ranming <53210441+ranming123456@users.noreply.github.com>
2024-10-11 09:34:54 +0800

    Create older_healthy_v1.py

---
+++ older_healthy_v1.py
@@ -0,0 +1,92 @@
+from sklearn.ensemble import RandomForestClassifier
+from sqlalchemy import create_engine
+import os
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+
+def database_connect():
+
+    user_mysql="root"
+    password_mysql="123456"
+    port_mysql=3306
+    host_mysql="192.168.109.103"
+    # database_mysql="data2"
+    database_mysql = "data_model_compete"
+    charset_mysql="utf8"
+    # 构造mysql连接
+    # conn = pymysql.connect(  user=user_mysql,password=password_mysql,host=host_mysql, port=port_mysql, database=database_mysql, charset=charset_mysql)
+    engine = create_engine('mysql+pymysql://%s:%s@%s:%s/%s?charset=utf8&autocommit=true'
+                           % (user_mysql, password_mysql, host_mysql, port_mysql, database_mysql)
+                           # ,pool_size=10,
+                           # pool_recycle=1600, pool_pre_ping=True,  pool_use_lifo=True,echo_pool=True
+                           , max_overflow=-1
+                           )
+    # 连接数据库
+    return engine
+
+def database_connect1():
+
+    user="user"
+    password="password"
+    port="port"
+    host="IP"
+    database = "your_database"
+    engine = create_engine('mysql+pymysql://%s:%s@%s:%s/%s?charset=utf8&autocommit=true' % (user, password, host, port, database))
+    return engine
+
+def execute_sql():
+    connection = database_connect()
+
+    sql = f"""
+            select * from (select *, ROW_NUMBER() over(partition  by id order by date desc ) as num  from older_healthy) t  where t.num=1
+            """
+    df = pd.read_sql(sql, connection)
+    df_x = df[["id", "feature1", "feature2", "feature3", "feature4", "feature5","gender"]]
+    df_y = df[["risk_target"]]
+
+    connection.connect().close()
+    return df_x,df_y
+
+def algorithm_rf(x,y):
+
+    scaler = StandardScaler()
+    x = pd.DataFrame(scaler.fit_transform(x))
+
+    rfc = RandomForestClassifier(n_estimators=100, criterion="entropy",random_state=55,bootstrap=False)
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3,shuffle=True, random_state=55)
+
+    rfc.fit(x_train, y_train)
+    rfc.feature_importances_
+    y_pred = rfc.predict(x_test)
+    score_pre = cross_val_score(rfc, x_test, y_test, cv=10).mean()
+    print(f" rf {score_pre}")
+    accuracy = accuracy_score(y_test, y_pred)
+
+def algorithm_svm(X,y):
+    scaler = StandardScaler()
+    X = pd.DataFrame(scaler.fit_transform(X))
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=15)
+    clf = SVC(kernel='rbf', random_state=1000)
+    clf.fit(X_train, y_train)
+
+    y_pred = clf.predict(X_test)
+
+    print("svm:", accuracy_score(y_test, y_pred,normalize=True))
+
+def rfc():
+    x, y = execute_sql()
+    algorithm_rf(x,y)
+
+def svm():
+    x, y = execute_sql()
+    algorithm_svm(x,y)
+
+if __name__=='__main__':
+    rfc()
+
